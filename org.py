9013630b3e2bd618917130107db6157d89626251
Adam Green <adam.green@adgefficiency.com>
2019-01-08 06:58:11 +1300

    linting

--- org.py
+++ org.py
@@ -1,22 +1,33 @@
+""" Calculates task densities using .json data """
 import json
+
 
 def load_json(file_name):
     return [json.loads(line) for line in open(file_name, 'r')]
 
-tasks = load_json('tasks.json')
-projects = load_json('projects.json')
 
-# print(tasks)
-# print(projects)
+def update_densities(tasks, projects):
+    for task in tasks:
+        task['priorities'] = [float(projects[project]['priority'])
+                              for project in task['projects']]
+        task['density'] = sum(task['priorities']) / float(task['cost'])
+    return tasks
 
-projects = {project['name']: project for project in projects}
 
-for task in tasks:
-    task['priorities'] = [float(projects[project]['priority'])
-                          for project in task['projects']]
+def sort_tasks(tasks):
+    tasks_dict = {task['name']: task for task in tasks}
+    tasks = sorted(
+        tasks_dict.items(), key=lambda tup: tup[1]['density'], reverse=True
+    )
+    return tasks
 
-    task['density'] = sum(task['priorities']) / float(task['cost'])
 
-tasks_dict = {task['name']: task for task in tasks}
+if __name__ == '__main__':
+    tasks = load_json('tasks.json')
+    projects = load_json('projects.json')
 
-tasks = sorted(tasks_dict.items(), key=lambda tup: tup[1]['density'], reverse=True)  
+    projects = {project['name']: project for project in projects}
+
+    tasks = update_densities(tasks, projects)
+
+    tasks = sort_tasks(tasks)
